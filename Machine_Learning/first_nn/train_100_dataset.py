1f4a2ca6d6c8d5bdd4a49160d7054b8f3479f913
mmihai <mihai.manole.95@gmail.com>
2019-03-13 23:22:14 +0200

    Update:saved file

--- Machine_Learning/first_nn/train_100_dataset.py
+++ Machine_Learning/first_nn/train_100_dataset.py
@@ -7,27 +7,18 @@ Created on Wed Mar 13 22:30:51 2019
 
 #%% IMPORTS
 #%%
-
-from pathlib import Path, PurePath
-
+import os
+from os.path import dirname, join
 #%% STATIC DEFINITIONS
 #%%
 
 training_file_name = "training_data/mnist_train_100.csv"
 
 
-
 #%% MAIN SECTION
 #%%
 
-path_to_file = Path(__file__).parents[1]
+d = dirname(os.getcwd())
+d = join(d, training_file_name)
 
-import os
-d = os.path.dirname(os.getcwd())
-print(d)
-d = os.path.join(d, training_file_name)
-print(d.__str__())
-#path_to_file = path.(path_to_file, training_file_name)
-#
-#print(path_to_file.joinpath(training_file_name))
 data_file = open(d.__str__().replace("\\", "/"), 'r')
