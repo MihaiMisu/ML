83636b8a6af2c9ab596aa4af37d8f9f5645cd3af
mmanole <mmanole@pentalog.com>
2019-04-13 14:32:11 +0300

    Working neuralNet model. Added requirements file.

--- Machine_Learning/first_nn/train_and_test_dataset.py
+++ Machine_Learning/first_nn/train_and_test_dataset.py
@@ -18,19 +18,29 @@ from git_nn import neuralNetwork as git_net
 
 #%% STATIC DEFINITIONS
 #%%
+"""
+It is required that the structure of the folders to be like the next one:
+    - MachineLearning:
+        - first_nn:
+            - test_data:
+                - mnist_test_10.csv
+                - mnist_test.csv
+            - training_data:
+                - mnist_train_100.csv
+                - mnist_train.csv
+"""
 
-training_file_name = "training_data/mnist_train_100.csv"
-training_file_name = "training_data/mnist_train.csv"
+#training_file_name = "first_nn/training_data/mnist_train_100.csv"
+training_file_name = "first_nn/training_data/mnist_train.csv"
 
-testing_file_name = "test_data/mnist_test_10.csv"
-testing_file_name = "test_data/test_data.csv"
+#testing_file_name = "first_nn/test_data/mnist_test_10.csv"
+testing_file_name = "first_nn/test_data/mnist_test.csv"
 
 wih_file_name = "my_net_wih.txt"
 who_file_name = "my_net_who.txt"
 
 #%% FUNCTIONS
 #%%
-
 def mapping(value_to_map, in_min, in_max, out_min, out_max):
     for i in range(len(value_to_map)):
         value_to_map[i] = ((value_to_map[i] - in_min) * (out_max - out_min)/ (in_max - in_min) + out_min)
@@ -55,17 +65,17 @@ testing_data = testing_data_file.readlines()
 testing_data_file.close()
 
 input_nodes = 784
-hidden_nodes = 200
+hidden_nodes = 100
 output_nodes = 10
 
-learning_rate = 0.5
+learning_rate = 0.3
 
 network = NeuralNetwork(input_nodes=input_nodes,
                         hidden_nodes=hidden_nodes,
                         output_nodes=output_nodes,
                         learning_rate=learning_rate)
 
-# TRAINING THE NETWORK
+# -------- TRAINING THE NETWORK -------------
 epochs = 1
 
 for e in range(epochs):
@@ -79,8 +89,8 @@ for e in range(epochs):
         targets[int(all_values[0])] = 0.99
         
         network.train(inputs, targets)
-
-# TESTING THE NETWORK
+# --------------------------------------------
+# --------- TESTING THE NETWORK --------------
 correct_result = 0
 for record in testing_data:
     # split the record by the ',' commas
@@ -97,9 +107,10 @@ for record in testing_data:
     if correct_label == nr:
         correct_result += 1
     
-    print(correct_label, "  ", nr)
+#    print(correct_label, "  ", nr)
+# --------------------------------------------
 
-print(correct_result / len(testing_data)*100)
+print("Detection rate: {} %".format(correct_result / len(testing_data)*100))
 
 #with open(d.__str__().replace("\\", "/"), 'r') as data_file:
 #    data_line = data_file.readline()
