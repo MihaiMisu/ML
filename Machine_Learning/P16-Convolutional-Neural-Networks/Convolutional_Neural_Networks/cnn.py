9a8a0e6f4d206881d265f34cd56deac581844811
mmanole <mmanole@pentalog.com>
2019-05-13 17:29:26 +0300

    ML-005. Defined CNN. Need optimisations to speed up the learning.

--- Machine_Learning/P16-Convolutional-Neural-Networks/Convolutional_Neural_Networks/cnn.py
+++ Machine_Learning/P16-Convolutional-Neural-Networks/Convolutional_Neural_Networks/cnn.py
@@ -9,7 +9,7 @@
 # Installing Keras
 # pip install --upgrade keras
 
-#%% Part 1 - Building the CNN
+#%% Part 1 - Import libraries
 #%%
 # Importing the Keras libraries and packages
 # used to implement the ANN
@@ -23,8 +23,10 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 # add fully connected layers
 from keras.layers import Dense
+# librari to preprocess the images
+from keras.preprocessing.image import ImageDataGenerator
 
-#%% Part 2 - initializing the CNN
+#%% Part 2 - Buid and initialise the CNN
 #%%
 
 classifier = Sequential()
@@ -39,16 +41,49 @@ classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation="relu"))
 # layer. In this way we reduce the no flattened layer
 classifier.add(MaxPooling2D(pool_size=(2, 2))) 
 
+# EXTRA: adding another conv layer and also pool it !!!
+classifier.add(Conv2D(32, (3, 3), activation="relu"))
+classifier.add(MaxPooling2D(pool_size=(2, 2))) 
+
 # Step 3 - flatteneing: taking all the pooled feature maps and reorganise it
 # into a single 1d vector which will be feeded to the fully connected network
 classifier.add(Flatten())
 
 # Step 4 - build the ANN and input the flattened vector to it
-classifier.add(Dense(output_dim=128, activation="relu"))
-classifier.add(Dense(output_dim=1, activation="sigmoid"))
+classifier.add(Dense(units=128, activation="relu"))
+classifier.add(Dense(units=1, activation="sigmoid"))
 
 # Compiling the CNN: optimizer - adam=stochastic descendent algorithm
 classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+
+#%% Part 3 - Training the network
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                    target_size=(64, 64),
+                                                    batch_size=64,
+                                                    class_mode='binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                                        target_size=(64, 64),
+                                                        batch_size=64,
+                                                        class_mode='binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch=8000,
+                         epochs=20,
+                         validation_data=test_set,
+                         validation_steps=2000)
+
+classifier.
 
 
 
