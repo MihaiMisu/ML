4add2b0f253be975478867b4dd0e5000f7e0ffc8
mmanole <mmanole@pentalog.com>
2019-10-08 19:33:07 +0300

    ConvNets. MNIST with convNets experiments

--- tensorflow_conv_nets/mnist_and_conv_net.py
+++ tensorflow_conv_nets/mnist_and_conv_net.py
@@ -19,13 +19,13 @@ KerasNetModel = TypeVar("NeuralNetModel")
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1), padding="valid", name="Conv1"))
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1), padding="valid", name="Conv_l1"))
+model.add(layers.MaxPooling2D((2, 2), name="MaxP_l1"))
 
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', name="Conv_l2"))
+model.add(layers.MaxPooling2D((2, 2), name="MaxP_l2"))
 
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', name="Conv_l3"))
 
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
