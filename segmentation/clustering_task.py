3fe7919568de55cfd64a035cbb72b2664c68685e
mmanole <mmanole@pentalog.com>
2019-11-22 14:56:31 +0200

    CD-0013. New module used. Pixels clustering examples.

---
+++ segmentation/clustering_task.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(username)s
+
+Script divided in multiple parts as it can be observed: Imports, Constants,
+etc. The Main Section part is even more divided - each subsection has to be
+run independently by the others to see its result
+
+"""
+
+# %%   Imports
+# %%
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage.color import rgb2gray
+from scipy import ndimage
+from sklearn.cluster import KMeans
+
+# %%   Constans
+# %%
+
+image_name = "1.jpeg"
+image_name = "bon.jpg"
+
+# %%   Classes & Functions
+# %%
+
+
+# %%    Main Section
+# %%
+
+image = plt.imread(image_name)
+image.shape
+plt.imshow(image)
+
+# ----------------------------------------
+
+gray = rgb2gray(image)
+plt.imshow(gray, cmap='gray')
+
+# ----------------------------------------
+
+gray_r = gray.reshape(gray.shape[0]*gray.shape[1])
+for i in range(gray_r.shape[0]):
+    if gray_r[i] > gray_r.mean():
+        gray_r[i] = 1
+    else:
+        gray_r[i] = 0
+gray = gray_r.reshape(gray.shape[0], gray.shape[1])
+plt.imshow(gray, cmap='gray')
+
+# ----------------------------------------
+
+image = plt.imread('edge_detect_image.png')
+plt.imshow(image)
+
+# ----------------------------------------
+
+# converting to grayscale
+gray = rgb2gray(image)
+
+# defining the sobel filters
+sobel_horizontal = np.array([np.array([1, 2, 1]), np.array([0, 0, 0]),
+                             np.array([-1, -2, -1])])
+print(sobel_horizontal, 'is a kernel for detecting horizontal edges')
+
+sobel_vertical = np.array([np.array([-1, 0, 1]), np.array([-2, 0, 2]),
+                           np.array([-1, 0, 1])])
+print(sobel_vertical, 'is a kernel for detecting vertical edges')
+
+out_h = ndimage.convolve(gray, sobel_horizontal, mode='reflect')
+out_v = ndimage.convolve(gray, sobel_vertical, mode='reflect')
+# here mode determines how the input array is extended when the filter overlaps
+# a border.
+plt.close('all')
+plt.subplot(131), plt.imshow(out_h, cmap='gray')
+plt.subplot(133), plt.imshow(out_v, cmap='gray')
+
+kernel_laplace = np.array([np.array([1, 1, 1]), np.array([1, -8, 1]),
+                           np.array([1, 1, 1])])
+print(kernel_laplace, 'is a laplacian kernel')
+out_l = ndimage.convolve(gray, kernel_laplace, mode='reflect')
+
+plt.subplot(132), plt.imshow(out_l, cmap='gray')
+
+# ----------------------------------------
+# K-means algorithm to segment an image
+
+pic = plt.imread(image_name)/255
+plt.close("all")
+plt.subplot(121), plt.imshow(pic)
+
+pic_n = pic.reshape(pic.shape[0]*pic.shape[1], pic.shape[2])
+kmeans = KMeans(n_clusters=5, random_state=0).fit(pic_n)
+pic2show = kmeans.cluster_centers_[kmeans.labels_]
+
+cluster_pic = pic2show.reshape(pic.shape[0], pic.shape[1], pic.shape[2])
+plt.subplot(122), plt.imshow(cluster_pic)
+
+# %%   Plotting section
+# %%
