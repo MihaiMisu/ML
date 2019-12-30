75b9024be1c6a90aa4d805eb409e7dbd1b0e9c66
MihaiMisu <mihai.manole.95@gmail.com>
2019-12-30 12:38:05 +0200

    CD-0013. Pytorch tutorial followed to segment human shape. TRained model on GPU-cuda.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-First commit
+
+
+Steps to install all requirements:
+1. install all modules found in requirements.txt file.
+2. for cocoapi tools there are some changes to be made depending on the os
+    - Linux: same as on repo (https://github.com/cocodataset/cocoapi.git)
+    - Win: setup.py file has to be changed, se replace:
+        - line: extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+	- with line: extra_compile_args={'gcc': ['/Qstd=c99']},
+    - make sure there are installed Cython (should be included in requirements file)
+    and C++ 14.0 (or higher) build tools => this last requirement is applicable only
+    for Win machines, no need on linux based.
+
+Observations: if there are some errors while isntalling PythonAPI, try to set the
+    right path, one of the following: cocoapi/ or cocoapi/PythonAPI
--- mak_rcnn_pytorch/mask_rcnn.py
+++ mak_rcnn_pytorch/mask_rcnn.py
@@ -5,9 +5,12 @@ import torch.utils.data
 import torchvision
 import utils
 import transforms as T
+import torchvision.transforms.functional as TF
 
+from torch import nn
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
 from PIL import Image
 from engine import train_one_epoch, evaluate
 
@@ -110,81 +113,103 @@ def get_transform(train):
     return T.Compose(transforms)
 
 
+def single_prediction(model, img_path):
+    """
+    Returns array of 1s and 0s which represents the pixel mask for what the
+    model detected.
+    """
+
+    img = Image.open(img_path)
+    img = TF.to_tensor(img)
+
+    model.eval()
+    with torch.no_grad():
+        prediction = model([img.to(device)])
+
+    return img, prediction
+
+
+def load_model_from_state_dict(model_path):
+
+    try:
+        # define an empty module used only to load a trained one
+        model = get_instance_segmentation_model(2)
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+    except Exception as ex:
+        print("Error in loading trained model. Possible not right predictions")
+        print(str(ex))
+
+    return model
+
 #    We now have the dataset class, the models and the data transforms. Let's
 # instantiate them
 
-# use our dataset and defined transformations
-dataset = PennFudanDataset('PennFudanPed', get_transform(train=True))
-dataset_test = PennFudanDataset('PennFudanPed', get_transform(train=False))
+if __name__ == '__main__':
+    # use our dataset and defined transformations
+    dataset = PennFudanDataset('PennFudanPed', get_transform(train=True))
+    dataset_test = PennFudanDataset('PennFudanPed', get_transform(train=False))
 
-# split the dataset in train and test set
-torch.manual_seed(1)
-indices = torch.randperm(len(dataset)).tolist()
-dataset = torch.utils.data.Subset(dataset, indices[:-50])
-dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+    # split the dataset in train and test set
+    torch.manual_seed(1)
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
 
-# define training and validation data loaders
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True, num_workers=4,
-    collate_fn=utils.collate_fn)
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=1, shuffle=True, num_workers=0,
+        collate_fn=utils.collate_fn)
 
-data_loader_test = torch.utils.data.DataLoader(
-    dataset_test, batch_size=1, shuffle=False, num_workers=4,
-    collate_fn=utils.collate_fn)
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
 
-#    Now let's instantiate the model and the optimizer
+    #    Now let's instantiate the model and the optimizer
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+#    device = torch.device('cpu')
+    # our dataset has two classes only - background and person
+    num_classes = 2
 
-# our dataset has two classes only - background and person
-num_classes = 2
+    # get the model using our helper function
+    model = get_instance_segmentation_model(num_classes)
+    # move model to the right device
+    model.to(device)
 
-# get the model using our helper function
-model = get_instance_segmentation_model(num_classes)
-# move model to the right device
-model.to(device)
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
 
-# construct an optimizer
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005,
-                            momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler which decreases the learning rate by
+    # 10x every 3 epochs
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
 
-# and a learning rate scheduler which decreases the learning rate by
-# 10x every 3 epochs
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                               step_size=3,
-                                               gamma=0.1)
+    # let's train it for 10 epochs
+    num_epochs = 3
 
-# let's train it for 10 epochs
-num_epochs = 10
-
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-    # update the learning rate
-    lr_scheduler.step()
-    # evaluate on the test dataset
-    evaluate(model, data_loader_test, device=device)
-
-
-
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
 
 
+    # pick one image from the test set
+    img, _ = dataset_test[0]
+    # put the model in evaluation mode
+    model.eval()
+    with torch.no_grad():
+        prediction = model([img.to(device)])
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    model = load_model_from_state_dict('pedestrian_model_dict_state.pt')
+    img, pred = single_prediction(model, 'PennFudanPed//test.png')
+    Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
+    Image.fromarray(pred[0]['masks'][0, 0].mul(255).byte().cpu().numpy())
 
